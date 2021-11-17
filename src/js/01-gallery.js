73086c1dc0565e0c408264b0db57a10aa1da7370
Aleksei211 <85795990+Aleksei211@users.noreply.github.com>
2021-11-17 22:10:43 +0200

    add task2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,31 +3,30 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-import '../css/01-gallery.css'
-import '../css/common.css'
+//import '../css/01-gallery.css'
+//import '../css/common.css'
 import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
-import 'lazysizes';
+//import 'lazysizes';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
 
 
-const galleryOfImages = document.querySelector('.gallery');
-const cardOfImage = onCreateGalleryItem(galleryItems);
+const gallery = document.querySelector('.gallery');
+const itemOfGallery = createItemOfGallery(galleryItems);
 
 
-galleryOfImages.insertAdjacentHTML('beforeend', cardOfImage);
+gallery.insertAdjacentHTML('beforeend', itemOfGallery);
 
-function onCreateGalleryItem(galleryItems) {
+function createItemOfGallery(galleryItems) {
     return galleryItems.map(({preview, original, description}) => {
         return `<a class="gallery__item" href="${original}">
                     <img class="gallery__image lazyload" data-src="${preview}" alt="${description}" />
                 </a> `
     }).join('')
 }
-// Interface
-new SimpleLightbox('.gallery a', {
-   disableRightClick: true,
-    scrollZoom: false,
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
     captionDelay: 250,
-    captionsData: 'alt', 
 });
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
 
+//var throttle = require(' lodash.throttle ')
+
+const STORAGE_KEY = "videoplayer-current-time";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// Фиксирует время с интервалом ~ 1000ms
+player.on('timeupdate', throttle(data => localStorage.setItem(STORAGE_KEY, data.seconds), 1000));
+// Задает плееру сохраненное время
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY))
